c6b65ece52a8532e6bc76d5f721fbf43ea44e39a
Austin Q. Truong <auqtruo@gmail.com>
2024-10-20 20:56:26 -0500

    Add login view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.pagination import PageNumberPagination
+from .serializers import UserSerializer, FoodSerializer, FoodDetailSerializer
+from .models import Food, DailyIntake, WeightTracker
 # Create your views here.
 
 #User view
@@ -11,4 +16,19 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
     
+#Login view; authenticate user and allow them to login; 200 for success and 400 for failure
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
+#List food view
+
+#Food nutrition/details view
