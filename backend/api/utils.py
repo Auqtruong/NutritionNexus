362cca5dd435c6cd961de28b58ee177b6d00b6a5
Austin Q. Truong <auqtruo@gmail.com>
2024-11-08 04:53:53 -0600

    Modify settings.py to correctly read from .env file; Refined API call/function in utils.py; added preliminary unit tests for food views

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,25 +1,36 @@
 import requests
+import logging
 from django.conf import settings
+from urllib.parse import quote
 
 #API Call
 def fetch_nutrition_data(user_query, api_key=settings.API_KEY):
+    if not user_query:
+        logging.error("Empty query provided")
+        return None
+    
     api_url = "https://api.calorieninjas.com/v1/nutrition?query="
-    query_url = f"{api_url}{user_query}"
+    query_url = f"{api_url}{quote(user_query)}"
     headers = {"X-Api-Key": api_key}
 
-    response = requests.get(query_url, headers=headers)
+    try:
+        response = requests.get(query_url, headers=headers)
+        response.raise_for_status()
 
-    if response.status_code == 200:
         data = response.json()
-        #if the food item has nutrition data to return
-        if data["items"]:
-            item = data["items"][0]
+        
+        items = data.get("items")
+        if items:
+            item = items[0]
             return {
                 "calories": item.get("calories", 0),
                 "carbohydrates": item.get("carbohydrates_total_g", 0.00),
                 "protein": item.get("protein_g", 0.00),
                 "fat": item.get("fat_total_g", 0.00),
             }
-    else:
-        print(f"Error when fetching nutrition data: {response.status_code}")
+        else:
+            logging.warning(f"No nutrition data found for query: {user_query}")
+            return None
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error when fetching nutrition data: {str(e)}")
         return None
